f8645b23312842d360c5febd9c910f4a4d85b065
Shubh <shubh@sas.upenn.edu>
2023-07-11 16:12:37 -0400

    add requirements

--- requirements.sh
+++ requirements.sh
@@ -1,2 +1,5 @@
 pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu121
 pip install torch_geometric
+pip install healpy h5py
+pip install datasets
+pip install ipykernel
